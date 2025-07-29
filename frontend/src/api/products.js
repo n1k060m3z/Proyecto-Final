a1635cd0483c497bcc5131d354d68373a74e32c7
Nicolas Gomez <118325793+n1k060m3z@users.noreply.github.com>
2025-07-28 22:59:15 -0500

    Eliminé todo desde main

--- frontend/src/api/products.js
+++
@@ -1,8 +0,0 @@
-import api from './axios';
-
-const API_URL = 'http://localhost:8000/api/productos/';
-
-export const getProducts = async () => {
-  const response = await api.get(API_URL);
-  return response.data;
-};
