c35f695204980d6945ae1673760561be25367ecc
DeinerLasprilla <deinerlasprilla@gmail.com>
2025-09-27 12:20:22 -0500

    modificacion login
    se agrego recuperar contraseña en login

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class Usuario(AbstractUser):
     es_vendedor = models.BooleanField(default=False)
     telefono = models.CharField(max_length=20, blank=True, null=True)
+    reset_token = models.CharField(max_length=64, blank=True, null=True)
 
     def __str__(self):
         return self.username
